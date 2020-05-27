8f545c4e6260fab8ef155a5ab69ff37cc458a7de
Matt Green <magreen118@gmail.com>
2020-05-27 19:15:43 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,6 +1,12 @@
+def find_max_value(array)
+  sorted_array = array.sort
+  return sorted_array[-1]
+end
 
+=begin
 #this is the easiest way to do it but they want you to do it manually(see above)
 def find_max_value(array)
   sorted_array = array.sort
   return sorted_array[-1]
 end
+=end
